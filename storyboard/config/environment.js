774c083323a46bdf57e6b131a34206cc36d0dfea
Curt Husting <curt.husting@cygnus.com>
2016-04-07 13:44:52 -0500

    setup firebase and torii

--- storyboard/config/environment.js
+++ storyboard/config/environment.js
@@ -4,6 +4,11 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'storyboard',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://storyboard-online.firebaseio.com/',
+    torii: {
+      sessionServiceName: 'session'
+    },
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
