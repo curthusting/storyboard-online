ea7bc32238c80d7791b7c82077f592cfb9113a62
Curt Husting <curt.husting@cygnus.com>
2016-04-08 11:49:54 -0500

    misc. updates

--- storyboard/app/routes/application.js
+++ storyboard/app/routes/application.js
@@ -19,6 +19,11 @@ export default Ember.Route.extend({
         console.log(data.currentUser);
       });
     },
+    social: function(provider) {
+      this.get("session").open("firebase", { provider: provider}).then(function(data) {
+        console.log(data.currentUser);
+      });
+    },
     signOut: function() {
       this.get("session").close();
     }
